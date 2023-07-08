455be7271158de0898888b2d02f1ce752c36e3bc
Ruslan Babkin <ruslan.babkin01@gmail.com>
2023-07-08 16:19:55 +0300

    add routing

--- app/about/head.tsx
+++ app/about/head.tsx
@@ -1,9 +1,9 @@
 export default function Head() {
   return (
     <>
-      <title>Create Next App</title>
+      <title>About Page</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
-      <meta name='description' content='Create by create next app' />
+      <meta name='description' content='Create by Ruslan' />
       <link rel='icon' href='/favicon.ico' />
     </>
   )
--- app/layout.tsx
+++ app/layout.tsx
@@ -6,13 +6,18 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body suppressHydrationWarning={true}>
+        <nav>
+          <h1>My NavBar</h1>
+        </nav>
+        {children}
+      </body>
     </html>
   )
 }
